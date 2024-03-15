3cfe3cf50180aa7212a7b2a8a924e5eb8d3efa58
Антон Яковлев <anton2yakovlev@gmail.com>
2024-03-15 23:02:27 +0300

    new types of requests

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
-from words import words
+from words import words_accordind_host, words_accordind_Dvinyatin, words_accordind_dictionary
 import random
 
 app = FastAPI()
 
+def get_response(words_list):
+    return {"word": random.choice(words_list)}
+
 @app.get("/get-so-word")
-def read_random_word():
-    return {"word": random.choice(words)}
+def get_random_word():
+    return get_response(words_accordind_dictionary)
+
+@app.get("/get-host-so-word")
+def get_random_host_word():
+    return get_response(words_accordind_host)
+
+@app.get("/get-dvinyatin-so-word")
+def get_random_dvinyatin_word():
+    return get_response(words_accordind_Dvinyatin)
