338b1163ac1c6da6bbdbd4ed4de8528a8ebb8e7d
Антон Яковлев <anton2yakovlev@gmail.com>
2024-03-15 23:19:27 +0300

    fix tests

--- test_main.py
+++ test_main.py
@@ -1,27 +1,27 @@
 # test_main.py
 import pytest
-from httpx import AsyncClient
+from httpx import ASGITransport, AsyncClient
 from main import app
 
-@pytest.mark.anyio
+@pytest.mark.asyncio
 async def test_get_random_word():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         response = await ac.get("/get-so-word")
         assert response.status_code == 200
         assert "word" in response.json()
         assert isinstance(response.json()["word"], str)
 
-@pytest.mark.anyio
+@pytest.mark.asyncio
 async def test_get_random_host_word():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         response = await ac.get("/get-host-so-word")
         assert response.status_code == 200
         assert "word" in response.json()
         assert isinstance(response.json()["word"], str)
 
-@pytest.mark.anyio
+@pytest.mark.asyncio
 async def test_get_random_dvinyatin_word():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         response = await ac.get("/get-dvinyatin-so-word")
         assert response.status_code == 200
         assert "word" in response.json()
