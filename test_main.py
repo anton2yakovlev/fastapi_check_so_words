a4232ac046eabdf2f2787b19dbaeab94fedc9d1d
Антон Яковлев <anton2yakovlev@gmail.com>
2024-03-15 23:05:55 +0300

    update tests

--- test_main.py
+++ test_main.py
@@ -1,10 +1,28 @@
+# test_main.py
 import pytest
 from httpx import AsyncClient
 from main import app
 
-@pytest.mark.asyncio
-async def test_read_random_word():
+@pytest.mark.anyio
+async def test_get_random_word():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.get("/get-so-word")
-    assert response.status_code == 200
-    assert "word" in response.json()
+        assert response.status_code == 200
+        assert "word" in response.json()
+        assert isinstance(response.json()["word"], str)
+
+@pytest.mark.anyio
+async def test_get_random_host_word():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/get-host-so-word")
+        assert response.status_code == 200
+        assert "word" in response.json()
+        assert isinstance(response.json()["word"], str)
+
+@pytest.mark.anyio
+async def test_get_random_dvinyatin_word():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/get-dvinyatin-so-word")
+        assert response.status_code == 200
+        assert "word" in response.json()
+        assert isinstance(response.json()["word"], str)
