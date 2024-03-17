a79a39220d00ee087cee020595857aac8ef6078c
Антон Яковлев <anton2yakovlev@gmail.com>
2024-03-17 22:03:22 +0300

    add readme to main page

--- model.py
+++ model.py
@@ -1,14 +1,17 @@
-from fastapi.responses import JSONResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 import random
 
 
-def form_correct_format(content):
+def form_html_format(content):
+    return HTMLResponse(content=content, status_code=200)
+
+def form_json_format(content):
     return JSONResponse(content=content, media_type="application/json; charset=utf-8")
 
 def get_word_response(words_list):
     content = {"word": random.choice(words_list)}
-    return form_correct_format(content)
+    return form_json_format(content)
 
 def get_words_list_response(words_list):
     content = {"words": words_list}
-    return form_correct_format(content)
+    return form_json_format(content)
