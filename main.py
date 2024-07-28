6b8ede12590a387f86a20ef7fee33c195cc4bacd
Anton Yakovlev <Kolomin58@gmail.com>
2024-07-28 22:13:49 +0300

    black formatter

--- main.py
+++ main.py
@@ -1,37 +1,48 @@
 from fastapi import FastAPI
 from markdown import markdown
 from model import form_html_format, get_word_response, get_words_list_response
-from words import words_accordind_host, words_accordind_Dvinyatin, words_accordind_dictionary
+from words import (
+    words_accordind_host,
+    words_accordind_Dvinyatin,
+    words_accordind_dictionary,
+)
 
 
 app = FastAPI()
 
+
 @app.get("/")
 def docs_redirect():
-    with open('README.md', 'r', encoding='utf-8') as md_file:
+    with open("README.md", "r", encoding="utf-8") as md_file:
         md_text = md_file.read()
-    html = markdown(md_text, extensions=['fenced_code'])
+    html = markdown(md_text, extensions=["fenced_code"])
     return form_html_format(content=html)
+
 
 @app.get("/get-so-word")
 def get_random_word():
     return get_word_response(words_accordind_dictionary)
 
+
 @app.get("/get-host-so-word")
 def get_random_host_word():
     return get_word_response(words_accordind_host)
+
 
 @app.get("/get-dvinyatin-so-word")
 def get_random_dvinyatin_word():
     return get_word_response(words_accordind_Dvinyatin)
 
+
 @app.get("/get-all-so-word")
 def get_all_words():
     return get_words_list_response(words_accordind_dictionary)
 
+
 @app.get("/get-all-host-so-word")
 def get_all_host_words():
     return get_words_list_response(words_accordind_host)
+
 
 @app.get("/get-all-dvinyatin-so-word")
 def get_all_dvinyatin_words():
--- model.py
+++ model.py
@@ -5,12 +5,15 @@ import random
 def form_html_format(content):
     return HTMLResponse(content=content, status_code=200)
 
+
 def form_json_format(content):
     return JSONResponse(content=content, media_type="application/json; charset=utf-8")
+
 
 def get_word_response(words_list):
     content = {"word": random.choice(words_list)}
     return form_json_format(content)
+
 
 def get_words_list_response(words_list):
     content = {"words": words_list}
